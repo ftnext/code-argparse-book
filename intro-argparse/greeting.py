6e8717959a2081fe57e79a81362525cb3550a909
ftnext <takuyafjp+develop@gmail.com>
2019-12-05 09:20:26 +0900

    [feat] 1-2 argparseを使って挨拶をするスクリプトを書き換え

--- intro-argparse/greeting.py
+++ intro-argparse/greeting.py
@@ -7,4 +7,7 @@ def hi(name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("name")
     args = parser.parse_args()
+    name = args.name
+    hi(name)
