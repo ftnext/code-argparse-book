566cca6c819f69d4884fa37d6d3a7a7976af9e65
ftnext <takuyafjp+develop@gmail.com>
2019-12-05 09:29:04 +0900

    [feat] 1-3 optional引数を試す

--- intro-argparse/greeting.py
+++ intro-argparse/greeting.py
@@ -7,8 +7,7 @@ def hi(name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("name")
-    parser.add_argument("variable")
+    parser.add_argument("--name")
     args = parser.parse_args()
     name = args.name
     hi(name)
