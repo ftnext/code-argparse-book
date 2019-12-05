888bf0655a7f484405ca16a2a6f2565685b6062f
ftnext <takuyafjp+develop@gmail.com>
2019-12-05 09:25:29 +0900

    [feat] 1-3 位置引数の実験の続き：位置引数の順番を変更

--- intro-argparse/greeting.py
+++ intro-argparse/greeting.py
@@ -7,8 +7,8 @@ def hi(name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("variable")
     parser.add_argument("name")
+    parser.add_argument("variable")
     args = parser.parse_args()
     name = args.name
     hi(name)
