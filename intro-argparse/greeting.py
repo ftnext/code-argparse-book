39d49d0e8c2473530b52087d050ae96d45e67852
ftnext <takuyafjp+develop@gmail.com>
2019-12-05 09:37:40 +0900

    [feat] Noneが渡る事象に対して、required引数を使ってコマンドラインから指定させる解決法

--- intro-argparse/greeting.py
+++ intro-argparse/greeting.py
@@ -7,7 +7,7 @@ def hi(name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--name")
+    parser.add_argument("--name", required=True)
     args = parser.parse_args()
     name = args.name
     hi(name)
