b41c3aeb7c85309024007e48707c4b08c644eb00
ftnext <takuyafjp+develop@gmail.com>
2019-12-05 09:23:32 +0900

    [feat] 1-3 位置引数を増やす実験

--- intro-argparse/greeting.py
+++ intro-argparse/greeting.py
@@ -7,6 +7,7 @@ def hi(name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("variable")
     parser.add_argument("name")
     args = parser.parse_args()
     name = args.name
