fa653c821fae3b63671d81538c56b256c1c589a5
ftnext <takuyafjp+develop@gmail.com>
2019-12-05 10:04:25 +0900

    [feat] 1-4 消費税込みの価格を計算するスクリプト（ただし文字列扱いのために計算できない）

---
+++ intro-argparse/tax_price.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+def calculate_total_price(price, rate):
+    included_tax_rate = 1 + rate / 100
+    total = int(price * included_tax_rate)
+    print(f"税込金額は{total}円です")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("price")
+    args = parser.parse_args()
+
+    price = args.price
+    calculate_total_price(price, 8)
