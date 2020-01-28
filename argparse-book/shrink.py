7c7892a5f90b9d0b0bcaa7eca0d35972f914b5f9
ftnext <takuyafjp+develop@gmail.com>
2020-01-29 01:01:20 +0900

    [refactor] ファイルが1つ指定された時に、画像であれば縮小した画像を作成するコードを修正
    * ファイルが複数指定された時を見据えて、対象ファイルのパスと保存先のパスを返す関数を追加（shrink_image関数に全て持たせない）

--- argparse-book/shrink.py
+++ argparse-book/shrink.py
@@ -7,23 +7,33 @@ from PIL import Image
 SHRINKED_LENGTH = 300
 
 
+def target_image_path_pair(src_path):
+    filename = src_path.name
+    if filename.endswith((".png", ".jpg")):
+        return {"src": src_path, "dest": filename}
+
+
+def shrink_image(src_path, dest_path, shrinked_length):
+    im = Image.open(src_path)
+    width, height = im.size
+    if width > shrinked_length and height > shrinked_length:
+        if width > height:
+            new_width = shrinked_length
+            new_height = int((shrinked_length / width) * height)
+        else:
+            new_width = int((shrinked_length / height) * width)
+            new_height = shrinked_length
+        resized_im = im.resize((new_width, new_height), Image.BICUBIC)
+        resized_im.save(dest_path)
+        print(f"画像を縮小しました: {dest_path}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("src", type=Path)
     args = parser.parse_args()
 
     src_path = args.src
-    filename = src_path.name
-    if filename.endswith((".png", ".jpg")):
-        im = Image.open(src_path)
-        width, height = im.size
-        if width > SHRINKED_LENGTH and height > SHRINKED_LENGTH:
-            if width > height:
-                new_width = SHRINKED_LENGTH
-                new_height = int((SHRINKED_LENGTH / width) * height)
-            else:
-                new_width = int((SHRINKED_LENGTH / height) * width)
-                new_height = SHRINKED_LENGTH
-            resized_im = im.resize((new_width, new_height), Image.BICUBIC)
-            resized_im.save(filename)
-            print(f"画像を縮小しました: {filename}")
+    target_pair = target_image_path_pair(src_path)
+    if target_pair:
+        shrink_image(target_pair["src"], target_pair["dest"], SHRINKED_LENGTH)
