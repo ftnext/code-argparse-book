d424b7a4d2c34f8c2a9da4b30c982208702ec8bf
ftnext <takuyafjp+develop@gmail.com>
2020-01-29 01:06:20 +0900

    [refactor] 関数が長いため細かい処理に切り出し（v1.0 3-3の関数を導入）

--- argparse-book/shrink.py
+++ argparse-book/shrink.py
@@ -7,23 +7,37 @@ from PIL import Image
 SHRINKED_LENGTH = 300
 
 
+def is_target_image(filename):
+    return filename.endswith((".png", ".jpg"))
+
+
 def target_image_path_pair(src_path):
     filename = src_path.name
-    if filename.endswith((".png", ".jpg")):
+    if is_target_image(filename):
         return {"src": src_path, "dest": filename}
+
+
+def needs_shrink(width_height_pair, limit):
+    width, height = width_height_pair
+    return width > limit and height > limit
+
+
+def shrink_size(width_height_pair, max_length):
+    width, height = width_height_pair
+    if width > height:
+        new_width = max_length
+        new_height = int((max_length / width) * height)
+    else:
+        new_width = int((max_length / height) * width)
+        new_height = max_length
+    return (new_width, new_height)
 
 
 def shrink_image(src_path, dest_path, shrinked_length):
     im = Image.open(src_path)
-    width, height = im.size
-    if width > shrinked_length and height > shrinked_length:
-        if width > height:
-            new_width = shrinked_length
-            new_height = int((shrinked_length / width) * height)
-        else:
-            new_width = int((shrinked_length / height) * width)
-            new_height = shrinked_length
-        resized_im = im.resize((new_width, new_height), Image.BICUBIC)
+    if needs_shrink(im.size, shrinked_length):
+        new_size = shrink_size(im.size, shrinked_length)
+        resized_im = im.resize(new_size, Image.BICUBIC)
         resized_im.save(dest_path)
         print(f"画像を縮小しました: {dest_path}")
 
