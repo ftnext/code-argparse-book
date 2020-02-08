18e3c7a19bdae583f01e6f0c14c550ab19c93c6b
ftnext <takuyafjp+develop@gmail.com>
2020-02-08 14:49:52 +0900

    [refactor] 関数を使って画像縮小処理を書き直し（可読性を上げる）

--- argparse-book/shrink.py
+++ argparse-book/shrink.py
@@ -29,16 +29,10 @@ def shrink_size(width_height_tuple, max_length):
 if __name__ == "__main__":
     src_path = Path("/Users/.../Downloads/pyconjp.jpg")  # 絶対パスを指定してください
     filename = src_path.name
-    if filename.endswith((".png", ".jpg")):
+    if is_target_image(filename):
         im = Image.open(src_path)
-        width, height = im.size
-        if width > SHRINKED_LENGTH and height > SHRINKED_LENGTH:
-            if width > height:
-                new_width = SHRINKED_LENGTH
-                new_height = int((SHRINKED_LENGTH / width) * height)
-            else:
-                new_width = int((SHRINKED_LENGTH / height) * width)
-                new_height = SHRINKED_LENGTH
-            resized_im = im.resize((new_width, new_height), Image.BICUBIC)
+        if needs_shrink(im.size, SHRINKED_LENGTH):
+            new_size = shrink_size(im.size, SHRINKED_LENGTH)
+            resized_im = im.resize(new_size, Image.BICUBIC)
             resized_im.save(filename)
             print(f"画像を縮小しました: {filename}")
